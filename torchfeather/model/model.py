651f617c9e7982defbab02b471e605104be9b0a6
Hamidreza Alikhani <ha.alikhani.k@gmail.com>
2026-09-24 10:04:12 -0700

    base module for DSV3 model

--- torchfeather/model/model.py
+++ torchfeather/model/model.py
@@ -11,92 +11,80 @@ from torchfeather.model.moe import FeedForward, MoE
 from torchfeather.model.rope import apply_rotary_emb, precompute_freqs_cis
 
 
-class Attention(nn.Module):
-    def __init__(self, model_args: DeepSeekV3ModelArgs):
-        super().__init__()
-
-        self.dim = model_args.dim # 2048
-        self.n_heads = model_args.n_heads # 16
-        self.q_lora_rank = model_args.q_lora_rank # 0
-        self.kv_lora_rank = model_args.kv_lora_rank # 512
-        self.qk_nope_head_dim = model_args.qk_nope_head_dim # 128
-        self.qk_rope_head_dim = model_args.qk_rope_head_dim # 64
-        self.qk_head_dim = (
-            model_args.qk_nope_head_dim + model_args.qk_rope_head_dim
-        )  # 128 + 64 = 192
-        self.v_head_dim = model_args.v_head_dim  # 128
-
-        # As stated in the DeepSeek V2 paper, this helps only reduce the activation memory, but doesn't influence the amount of cache.
-        # We won't be using it.
-        if self.q_lora_rank == 0:
-            self.wq = nn.Linear(self.dim, self.n_heads * self.qk_head_dim, bias=False) # shape: (n_heads * qk_head_dim, dim)
-        else:
-            self.wq_a = nn.Linear(self.dim, self.q_lora_rank, bias=False)
-            self.q_norm = nn.RMSNorm(self.q_lora_rank, eps=model_args.norm_eps)
-            self.wq_b = nn.Linear(
-                self.q_lora_rank, self.n_heads * self.qk_head_dim, bias=False
-            )
-
-        self.wkv_a = nn.Linear(
-            self.dim, self.kv_lora_rank + self.qk_rope_head_dim, bias=False
-        )
-        self.kv_norm = nn.RMSNorm(self.kv_lora_rank, eps=model_args.norm_eps)
-        self.wkv_b = nn.Linear(
-            self.kv_lora_rank, 
-            self.n_heads * (self.qk_nope_head_dim + self.v_head_dim),
-            bias=False,
-        )
-        self.wo = nn.Linear(
-            self.n_heads * self.v_head_dim,
-            self.dim,
-            bias=False,
-        )
-        self.softmax_scale = self.qk_head_dim ** -0.5
-
-        if model_args.max_seq_len > model_args.original_seq_len:
-            mscale = 0.1 * model_args.mscale * math.log(model_args.rope_factor) + 1.0
-            self.softmax_scale = self.softmax_scale * mscale * mscale
-
-        self.inner_attention = ScaledDotProductAttentionWrapper()
-
-    def forward(
-        self,
-        x: torch.Tensor,
-        freqs_cis: torch.Tensor,
-    ):
-        raise NotImplementedError("Not implemented")
-
-    def init_weights(
-        self,
-        init_std: float,
-        buffer_device: torch.device | None = None,
-        ):
-        linear_list = [
-            self.wkv_a,
-            self.wkv_b,
-        ]
-        if self.q_lora_rank > 0:
-            linear_list.extend([self.wq_a, self.wq_b])
-        else:
-            linear_list.append(self.wq)
-
-        for linear in linear_list:
-            nn.init.trunc_normal_(linear.weight, mean=0.0, std=0.02)
-        nn.init.trunc_normal_(self.wo.weight, mean=0.0, std=init_std)
-
-        self.kv_norm.reset_parameters()
-        if self.q_lora_rank > 0:
-            self.q_norm.reset_parameters()
-
-
 
 class DeepSeekV3Model(nn.Module):
     def __init__(self, model_args: DeepSeekV3ModelArgs):
-        raise NotImplementedError("Not implemented")
+        super().__init__()
+        self.model_args = model_args
+        self.tok_embeddings = nn.Embedding(model_args.vocab_size, model_args.d_model)
+        self.register_buffer(
+            "freqs_cis", precompute_freqs_cis(model_args), persistent=False
+        ) # persis = False means that the buffer is not saved to the checkpoint (not in the state_dict)
+
+        self.layers = nn.ModuleDict()
+        for layer_id in range(model_args.n_layers):
+            self.layers[str(layer_id)] = TransformerBlock(layer_id, model_args)
+
+        self.norm = nn.RMSNorm(model_args.dim)
+        self.output = nn.Linear(
+            model_args.dim,
+            model_args.vocab_size,
+            dtype=torch.default_dtype,
+            bias=False,
+        )
 
 
+    def init_weights(
+        self,
+        init_std: float | None = None,
+        buffer_device: torch.device | None = None,
+    ):
+        buffer_device = buffer_device or self.freqs_cis.device
+        with torch.device(buffer_device):
+            self.freqs_cis = precompute_freqs_cis(self.model_args)
+        
+        if self.tok_embeddings is not None:
+            nn.init.normal_(self.tok_embeddings.weight)
 
+        for layer in self.layers.values():
+            if layer is not None:
+                layer.init_weights(init_std=init_std, buffer_device=buffer_device)
 
+        if self.norm is not None:
+            self.norm.reset_parameters()
+
+        final_out_std = self.model_args.dim**-0.5
+        cutoff_factor = 3
+        if self.output is not None:
+            nn.init.trunc_normal_(
+                self.output.weight,
+                mean=0.0,
+                std=final_out_std,
+                a=-cutoff_factor * final_out_std,
+                b=cutoff_factor * final_out_std,
+            )
+
+        
+    def forward(self, tokens: torch.Tensor):
+        """
+        Forward pass for the Transformer model.
+
+        Args:
+            tokens (torch.Tensor): Input token indices if pipeline parallelism is not enabled.
+                If pipeline parallelism is enabled, this will be the input token indices for the ranks on the first pipeline stage. This will be the activation of the previous pipeline stage if the current rank is not on the first stage.
+
+        Returns:
+            torch.Tensor: Logits tensor of shape (batch_size, vocab_size).
+        """
+        h = self.tok_embeddings(tokens) if self.tok_embeddings is not None else tokens
+
+        for layer in self.layers.values():
+            h = layer(h, freqs_cis=self.freqs_cis)
+        
+        h = self.norm(h) if self.norm is not None else h
+        logits = self.output(h) if self.output is not None else h 
+
+        return logits
 
 
 
